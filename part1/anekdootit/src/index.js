5341b1e67f76a3dfe013fb070b2a18f03b88e15e
Peterii <petteri.sallstrom@hotmail.com>
2020-04-05 13:09:41 +0300

    Completed step 1

---
+++ part1/anekdootit/src/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
+
+  const randomAnecdote = () => {
+    setSelected(Math.floor(Math.random() * 6));
+  };
+
+  return (
+    <>
+      <button onClick={randomAnecdote}>Press me to random the anecdote!</button>
+      {props.anecdotes[selected]}
+    </>
+  );
+};
+
+const anecdotes = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+];
+
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById("root"));
